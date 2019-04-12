48437b155257af8bf8a4dcd8ad53865b5c9b00e7
hahyun8587 <hahyun8587@gamil.com>
2019-04-12 11:22:03 +0900

    merge please

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void sub1();
+void sub1(int *arr, int n);
 void sub2();
 
 int main(){
@@ -8,5 +8,24 @@ sub1();
 sub2();
 
 return 0;
+}
+
+int sub1(int *arr, int n)
+{
+    int min_index;
+    int temp;
+    int i;
+
+    for (i = 0; i < n - 1; i++) {
+	min_index = 0;
+
+	for (j = i + 1; j < n; j++) {
+	    if (arr[j] < arr[min_index])
+		min_index = j;
+	}
+        temp = arr[i];
+	arr[i] = arr[min_index];
+	arr[min_index] = temp; 
+    } 
 }
 
