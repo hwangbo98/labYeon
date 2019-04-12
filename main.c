74e387756592908b2778a5ee84e990812a474c3f
hwangbo98 <hwangbo1998@gmail.com>
2019-04-12 11:07:29 +0900

    sub1

--- main.c
+++ main.c
@@ -9,4 +9,11 @@ sub2();
 
 return 0;
 }
-
+void sub1(){
+    int i int j;
+    for(i=1; i<10; i++){
+        for(j=1; j<10; j++){
+            printf("%d * %d = %d",i,j,i*j);
+        }
+    }
+}   
